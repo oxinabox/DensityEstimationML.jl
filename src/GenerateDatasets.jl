6681773ae72338aca89008a8c6f63fdc01edfdc3
oxinabox <oxinabox@ucc.asn.au>
2017-09-12 16:46:28 +0800

    =switched to creating singleton distributions

--- src/GenerateDatasets.jl
+++ src/GenerateDatasets.jl
@@ -6,15 +6,14 @@ This module contains functiosn for generating datasets suitable for testing dens
 module GenerateDatasets
 
 using Distributions
-using StaticArrays
-
+using StatsBase
 import DensityEstimationML: sample_from_cdf
 
 export approximate_support
 
 struct RectangularInterval{T}
-	lb::T
-	ub::T
+    lb::T
+    ub::T
 end
 
 Base.minimum(iv::RectangularInterval) = iv.lb
@@ -43,14 +42,17 @@ for their SIC they additionally tested with n=200
 
 This dataset is not conditional.
 """
-function magdon_ismail_and_atiya()
-	comp1 = Normal(-30, 3)
-	comp2 = Normal(9, 9)
-	MixtureModel([comp1, comp2], [0.3, 0.7])
+struct MagdonIsmailAndAtiya <: UnivariateDistribution{Continuous}
 end
 
-approximate_support(::typeof(magdon_ismail_and_atiya)) = RealInterval(-50.0, 50.0)
-Base.rand(distfun::typeof(magdon_ismail_and_atiya),  n=200) = rand(distfun(), n)
+function backing_distribition(::MagdonIsmailAndAtiya)
+        comp1 = Normal(-30, 3)
+        comp2 = Normal(9, 9)
+        MixtureModel([comp1, comp2], [0.3, 0.7])
+end
+
+approximate_support(::MagdonIsmailAndAtiya) = RealInterval(-50.0, 50.0)
+Base.rand(d::MagdonIsmailAndAtiya,  n=200) = rand(backing_distribition(d), n)
 
 
 """
@@ -61,12 +63,43 @@ They only estimate it between -12 and 12
 
 This dataset is not conditional.
 """
-function likas_1()
-	MixtureModel([Normal(-7,0.5), Uniform(-3,-1), Uniform(1,3), Normal(7,0.5)])
+struct Likas1 <: UnivariateDistribution{Continuous}
 end
 
-approximate_support(::typeof(likas_1)) = RealInterval(-12.0,12.0)
-Base.rand(distfun::typeof(likas_1),  n=5000) = rand(distfun(), n)
+function backing_distribition(::Likas1)
+    MixtureModel([Normal(-7,0.5), Uniform(-3,-1), Uniform(1,3), Normal(7,0.5)])
+end
+approximate_support(::Likas1) = RealInterval(-12.0,12.0)
+Base.rand(d::Likas1,  n=5000) = rand(backing_distribition(d), n)
+
+
+for wrapper in [:MagdonIsmailAndAtiya, :Likas1]
+    for (mod,funs) in [(:Distributions, [:sampler, :mode, :modes]),
+                       (:Base, [:minimum, :maximum, :mean, :var]),
+                       (:StatsBase, [:skewness])
+                      ]
+        for ff in funs
+            @eval $mod.$ff(d::$wrapper)=$mod.$ff(backing_distribition(d))
+        end
+    end
+    for (mod,funs) in [(:Distributions, [:pdf, :logpdf, :cdf, :insupport]),
+                       (:Base, [:quantile]),
+                       (:StatsBase, [:entropy])
+                      ]
+        for ff in funs
+            @eval $mod.$ff(d::$wrapper, x::Real)=$mod.$ff(backing_distribition(d), x)
+        end
+    end
+    
+    for (mod,funs) in [(:Distributions, [:mgf, :cf])]
+        for ff in funs
+            @eval $mod.$ff(d::$wrapper, x)=$mod.$ff(backing_distribition(d), x)
+        end
+    end
+   
+    @eval StatsBase.kurtosis(d::$wrapper, x::Bool)=StatsBase.kurtosis(backing_distribition(d),x)
+end
+
 
 
 """
@@ -78,9 +111,9 @@ Modha, D. S. & Fainman, 1994 Y. A learning law for density estimation
 The PDF is given by:
 
 \[f(x)=\begin{cases}
-    \frac{2-\frac{x}{2}}{6.5523}	& 0\le x<2\\
-    \frac{2-(x-3)^{2}}{6.5523}		& 0\le x<3+\sqrt{2}\\
-    0					& otherwise
+    \frac{2-\frac{x}{2}}{6.5523}    & 0\le x<2\\
+    \frac{2-(x-3)^{2}}{6.5523}      & 0\le x<3+\sqrt{2}\\
+    0                   & otherwise
 \end{cases}
 \]
 
@@ -97,29 +130,45 @@ F(x)=\dfrac{1}{6.5523}\begin{cases}
 Both works use the same 5000 samples
 This dataset is not conditional.
 """
-likas_2()=nothing
+struct Likas2<:UnivariateDistribution{Continuous}
+end
 
-function Distributions.cdf(::typeof(likas_2), x)
+function Distributions.pdf(::Likas2, x::Real)
     1/6.5523 * if (x ≤ 0)
-	0.0
+    0.0
     elseif (0<x ≤ 2)
-	2x-x^2/4
+    2.0-x/2
     elseif (2<x≤3+√2)
-	-x^3/3 + 3x^2 -7x + 23/3
+    2.0-(x-3.0)^2
     else
-	@assert(3+√2 < x)
-	6.5523
+    @assert(3+√2 < x)
+    0.0
     end
 end
 
-function Base.rand(::typeof(likas_2), n=5000)
-	likas_2_cdf(x) = cdf(likas_2, x)
-	[sample_from_cdf(likas_2_cdf, 2.5) for _ in 1:n]
+
+function Distributions.cdf(::Likas2, x::Real)
+    1/6.5523 * if (x ≤ 0)
+    0.0
+    elseif (0<x ≤ 2)
+    2x-x^2/4
+    elseif (2<x≤3+√2)
+    -x^3/3 + 3x^2 -7x + 23/3
+    else
+    @assert(3+√2 < x)
+    6.5523
+    end
 end
 
-Distributions.support(::typeof(likas_2)) = RealInterval(-1.0, 6.0)
+function Base.rand(::Likas2, n=5000)
+    likas_2_cdf(x) = cdf(likas_2, x)
+    [sample_from_cdf(likas_2_cdf, 2.5) for _ in 1:n]
+end
 
-const modha_and_fainman=likas_2
+Base.minimum(::Likas2) = -1.0
+Base.maximum(::Likas2) = 6.0
+
+const ModhaAndFainman=Likas2
 
 
 
@@ -135,9 +184,10 @@ Likas used 5000 samples.
 
 This dataset is not conditional.
 """
-likas_3()=nothing
+struct Likas3
+end
 
-Distributions.support(::typeof(likas_3))=RectangularInterval([0., 0.],[0.2, 0.2])
-Base.rand(::typeof(likas_3), n=200) = 0.2 .* rand((2, n))
+Distributions.support(::Likas3)=RectangularInterval([0., 0.],[0.2, 0.2])
+Base.rand(::Likas3, n=200) = 0.2 .* rand((2, n))
 
 end #module
